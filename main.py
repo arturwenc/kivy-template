ee09a0b1cd7553dfdd49ef34d8c94898e2e9607e
Artur Wenc <artur.wenc.dev@gmail.com>
2020-09-18 12:54:09 +0100

    implemeting user signup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 Builder.load_file('design.kv')
 
@@ -11,7 +13,16 @@ class RootWidget(ScreenManager):
     pass
 
 class SignUpScreen(Screen):
-    pass
+    def add_user(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+
+
+        users[uname] = {'username': uname, 'password': pword,
+        'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
 
 class MainApp(App):
     def build(self):
